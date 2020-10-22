41a0df281745d40736bf0ee68fa3cbb7b38fbcc4
Nika1411 <niksel99.99@mail.ru>
2020-10-23 02:31:43 +0300

    Общее задание

---  user.py
+++  user.py
@@ -1,3 +1,9 @@
+# Напишите программу (файл user.py), которая запрашивала бы у пользователя:
+# его имя (например, "What is your name?")
+# возраст ("How old are you?")
+# место жительства ("Where are you live?")
+# После этого выводила бы три строки:
+
 name = input("What is your name? ")
 age = input("How old are you? ")
 home = input("Where are you live? ")
--- numbers.py
+++ numbers.py
@@ -1,3 +1,7 @@
+# Запросите у пользователя четыре числа (файл numbers.py). Отдельно сложите первые два и
+# отдельно вторые два. Разделите первую сумму на вторую. Выведите результат на экран так,
+# чтобы ответ содержал две цифры после запятой.
+
 a = int(input("Enter the first number :"))
 b = int(input("Enter the following number:"))
 c = int(input("Enter the following number:"))
